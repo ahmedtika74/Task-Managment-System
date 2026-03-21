de92ca4e26d4f1a6841434dda2fb082340480c7b
ahmedtika74 <tekaliker@gmail.com>
2026-03-21 20:52:23 +0200

    feat: implement menu button to open side bar

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,13 +1,21 @@
+import { Menu } from "lucide-react";
 import Button from "../ui/Button";
 import useTheme from "../../hooks/useTheme";
 
-export default function Header() {
+export default function Header({ onClick }) {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className="flex h-16 items-center justify-between border-b px-6 dark:border-gray-700 dark:bg-gray-800">
-      <h2>Welcome Back, User!</h2>
-      <Button onClick={toggleTheme}>{theme === "dark" ? "🌙" : "☀️"}</Button>
-    </div>
+    <>
+      <div className="flex items-center justify-between border-b px-6 py-3 dark:border-gray-700 dark:bg-gray-800">
+        <div className="flex items-center justify-start">
+          <button className="p-2 md:hidden" onClick={onClick}>
+            <Menu size={24} />
+          </button>
+          <h2>Welcome Back, User!</h2>
+        </div>
+        <Button onClick={toggleTheme}>{theme === "dark" ? "🌙" : "☀️"}</Button>
+      </div>
+    </>
   );
 }
--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 
 export default function MainLayout() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className="flex h-screen overflow-hidden bg-gray-50 text-gray-900 dark:bg-gray-900 dark:text-white">
       <div>
-        <Sidebar />
+        <Sidebar
+          isOpen={isSidebarOpen}
+          onClose={() => setIsSidebarOpen(false)}
+        />
       </div>
       <div className="flex flex-1 flex-col">
-        <Header />
+        <Header onClick={() => setIsSidebarOpen(true)} />
         <main className="overflow-y-auto p-6">
           <Outlet />
         </main>
