7e42ed19434c859151f121ab8dfeb62729a2d96d
ahmedtika74 <tekaliker@gmail.com>
2026-03-21 18:29:19 +0200

    feat: Create reusable Sort Dropdown

---
+++ src/components/ui/SortDropdown.jsx
@@ -0,0 +1,19 @@
+export default function SortDropdown({ currentSort, onSortChange }) {
+  return (
+    <div>
+      <label>Sort By: </label>
+      <select
+        value={currentSort}
+        onChange={(e) => {
+          onSortChange(e.target.value);
+        }}
+        name="sort"
+        className="w-full cursor-pointer rounded-xl border border-gray-200 bg-white px-4 py-2 text-gray-700 transition-all focus:outline-0 sm:w-auto dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200"
+      >
+        <option value="date">Date</option>
+        <option value="title">Title</option>
+        <option value="priority">Priority</option>
+      </select>
+    </div>
+  );
+}
