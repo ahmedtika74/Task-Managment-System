9d81c6c2fe4bb134924c8b6d34d6e7cdb45523e6
ahmedtika74 <tekaliker@gmail.com>
2026-03-21 17:32:50 +0200

    feat: Create reusable search bar

---
+++ src/components/ui/SearchBar.jsx
@@ -0,0 +1,16 @@
+import { Search } from "lucide-react";
+
+export default function SearchBar({ value, onChange }) {
+  return (
+    <div className="flex w-full items-center justify-between rounded-xl border border-gray-200 bg-gray-100 px-5 dark:border-gray-700 dark:bg-gray-800">
+      <Search size={20} className="text-gray-400" />
+      <input
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        type="text"
+        placeholder="Search Tasks..."
+        className="ml-3 w-full py-2 pr-4 focus:outline-0"
+      />
+    </div>
+  );
+}
