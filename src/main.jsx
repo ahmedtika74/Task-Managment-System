b9769184010415253386f6d865469e6459bd63ac
ahmedtika74 <tekaliker@gmail.com>
2026-03-22 20:54:24 +0200

    feat: add toaster from react hot toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { Toaster } from "react-hot-toast";
+import App from "./App.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
+      <Toaster position="bottom-right" />
       <App />
     </Provider>
   </StrictMode>,
