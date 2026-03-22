dda70725105494ddb641a1d8f5b770a24112d48f
ahmedtika74 <tekaliker@gmail.com>
2026-03-22 20:54:37 +0200

    remove app.css , not used

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
 import Dashboard from "./pages/Dashboard";
